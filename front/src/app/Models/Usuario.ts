85b0ddf506079f6dc7268bdc054da44495d4d9bc
Iván Tortosa <shadowheroe03@gmail.com>
2024-02-16 20:19:55 +0100

    service creado en el front

---
+++ front/src/app/Models/Usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    id: Number
+    nombre: String
+    apellidoUno: String
+    apellidoDos: String
+    correo: String
+}
---
+++ front/src/app/service/usuario.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../Models/Usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private BASE_URL:string = "http://localhost:8080/api/"
+
+  private httpHeader: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
+
+  constructor(private http:HttpClient) { }
+
+  getUsuario(): Observable<Usuario[]>{
+    return this.http.get<Usuario[]>(this.BASE_URL + "/usuario")
+  }
+
+  guardarUsuario(usuario: Usuario): Observable<any> {
+    return this.http.post<any>(this.BASE_URL+"/alumno", usuario, {headers:this.httpHeader})
+  }
+
+}
